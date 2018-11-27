062ce7dd3c1edfd9ae02e7a5f8025a99a6693897
frank.gitonga <frank.gitonga@incentrocom>
2018-11-27 13:48:41 +0300

    Initial commit

---
+++ Music App/GenreButtonScreen.swift
@@ -0,0 +1,48 @@
+//
+//  GenreButtonScreen.swift
+//  Music App
+//
+//  Created by Frank on 11/20/18.
+//  Copyright © 2018 Qazana. All rights reserved.
+//
+
+import UIKit
+import MediaPlayer
+
+class GenreButtonScreen: UIViewController {
+    
+    var musicPlayer = MPMusicPlayerController.applicationMusicPlayer
+    
+    @IBAction func genreButtonTapped(_ sender: UIButton) {
+        MPMediaLibrary.requestAuthorization { (status) in
+            if status == .authorized {
+               self.playGenre(genre: sender.currentTitle! )
+            }
+        }
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        musicPlayer.stop()
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+        musicPlayer.skipToNextItem()
+    }
+    
+    func playGenre(genre: String ) {
+        musicPlayer.stop()
+        
+        let query = MPMediaQuery()
+        let predicate = MPMediaPropertyPredicate(value: genre, forProperty: MPMediaItemPropertyGenre)
+        
+        query.addFilterPredicate(predicate)
+        
+        musicPlayer.setQueue(with: query)
+        musicPlayer.shuffleMode = .songs
+        musicPlayer.play()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
